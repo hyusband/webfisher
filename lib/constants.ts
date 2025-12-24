6130832b305d8b73e5f70380d6bb5c1795a727e7
Andres <zaidmiguelhernandez@gmail.com>
2025-12-24 16:19:06 -0400

    feat: add download configuration constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://pgpxnscjkpacknuscqbg.supabase.co/storage/v1/object/public/downloads/WebFisher_x86%202.1.1.msi",
-    filename: "WebFisher_x86 2.1.1.msi"
+    url: "https://store8.gofile.io/download/web/18d2ed2c-a141-4e19-96c4-daa52ac535df/webfisher-2.1.0.zip",
+    filename: "webfisher-2.1.0.zip"
 };
