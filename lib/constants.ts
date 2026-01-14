bd56c92ffc39605de4b538bd3564a8d80790c56d
Andres <zaidmiguelhernandez@gmail.com>
2026-01-13 21:13:44 -0400

    Update download URL for WebFisher installer

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://322.lat/downloads/WebFisher_x86%20Setup%202.1.1.exe",
+    url: "https://store9.gofile.io/download/web/01e9cf5e-ff3e-4898-bd98-6b2db1c2d21d/WebFisher_x86%20Setup%202.1.1.msi",
     filename: "WebFisher_x86 Setup 2.1.1"
 };
