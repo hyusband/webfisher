a418f681f2dfc2b0e6ed87188d3ea4a819bfc908
Andres <zaidmiguelhernandez@gmail.com>
2026-01-17 19:19:33 -0400

    Update download URL and filename for WebFisher

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://cdn.discordapp.com/attachments/1460573180333920276/1460810064624619571/WebFisher_x86_Setup_2.1.1.msi.zip?ex=696844e3&is=6966f363&hm=4b516c31defc82bb5e8c48fa343b4f6fc31de9959aff1804832fc939bf401767&",
-    filename: "WebFisher_x86 Setup 2.1.1"
+    url: "https://download1523.mediafire.com/nrgjv44etibgi3C8irNazyOmEDkPCY-KIzCqSvNHinE5790elckrDQtrZ7BqFglukOwHB34NlEcz-pZtUoSs_eAPcVTuk43jxZ-t5UVenx4q5jzQLd915XTEkZ5B2r-xIh9Gso0LvKMSgb95D6VafjRNzHfBIVH_g9cjsdvTNydz/d4iljim6y9h1rso/WebFisher_Online+Setup+1.0.0%282%29.exe",
+    filename: "WebFisher_Online Setup 1.0.0"
 };
