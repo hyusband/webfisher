c91d30084f971a31714b7b6d71dfc887e12e8d37
Andres <zaidmiguelhernandez@gmail.com>
2026-01-22 20:41:03 -0400

    Update download configuration URLs and filenames

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://download1523.mediafire.com/nrgjv44etibgi3C8irNazyOmEDkPCY-KIzCqSvNHinE5790elckrDQtrZ7BqFglukOwHB34NlEcz-pZtUoSs_eAPcVTuk43jxZ-t5UVenx4q5jzQLd915XTEkZ5B2r-xIh9Gso0LvKMSgb95D6VafjRNzHfBIVH_g9cjsdvTNydz/d4iljim6y9h1rso/WebFisher_Online+Setup+1.0.0%282%29.exe",
-    filename: "WebFisher_Online Setup 1.0.0"
+    url: "https://322.lat/downloads/WebFisher_x86%20Setup%202.1.1.exe",
+    filename: "WebFisher_x86 Setup 2.1.1"
 };
