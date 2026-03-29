e0f993e492b11e1ba680a0f5ee60d0fd301569e0
Andres <zaidmiguelhernandez@gmail.com>
2026-03-29 01:06:44 -0300

    www

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.dropbox.com/scl/fi/70xrf14jutbln93d1vmdy/WebFisher_x86-Setup-2.1.1.exe?rlkey=fkck2dx2igd1juxkyeup3ijm4&e=1&st=6fi37tiz&dl=1",
+    url: "https://www.dropbox.com/scl/fi/r6rvmytu1wh3grbjpj1sq/WebFisher-Installer.7z?rlkey=egjdtlhnjw6m2f435s92kl9fk&st=va142wcz&dl=1",
     filename: "WebFisher_x86 Setup 2.1.1"
 };
