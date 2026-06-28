046a6ed6da5acf2a786b2de2b30b65e4a9b083e0
hyusband <zaidmiguelhernandez@gmail.com>
2026-06-28 12:27:07 -0400

    fix: update download URL in DOWNLOAD_CONFIG for correct file access

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.dropbox.com/scl/fi/colndapwetrdj0s0bmn1l/Webfisher-Setup-1.2.5.exe?rlkey=rn9n0akukte7l2tnn2q8xoxpg&st=eh28ylxp&dl=1",
+    url: "https://www.dropbox.com/scl/fi/t7wgo7ifvhrglmh9wsl68/Webfisher-Setup-1.2.5.exe?rlkey=2on7w9tv9bzc8y5yjh7b5r2t1&st=273orh9c&dl=1",
     filename: "Webfisher-Setup-1.2.5.exe"
 };
