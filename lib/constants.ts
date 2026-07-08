b5c1deb10e3f47d10cf4b230c65f0eceee41125b
Andres <zaidmiguelhernandez@gmail.com>
2026-07-07 20:18:07 -0400

    Update download URL to include direct link parameter

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.dropbox.com/scl/fi/ashj66n0pe8qo88qe2vpw/Webfisher-Setup-1.2.5.exe?rlkey=pg8qjmuwe5gr6s5o70tex778m&st=1je0j0pd&dl=",
+    url: "https://www.dropbox.com/scl/fi/ashj66n0pe8qo88qe2vpw/Webfisher-Setup-1.2.5.exe?rlkey=pg8qjmuwe5gr6s5o70tex778m&st=1je0j0pd&dl=1",
     filename: "Webfisher-Setup-1.2.5.exe"
 };
