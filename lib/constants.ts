59b1448a8b48841047ed56255e62ea6a4e9f6218
zlwar <meminm052@gmail.com>
2026-07-07 17:13:05 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.dropbox.com/scl/fi/kpqtpk9qeu9cqa7qvyirw/Webfisher-Setup-1.2.5.exe?rlkey=zcgy9drfr0gi0gdw090f6hzvl&st=5onzwuq9&dl=1",
+    url: "https://www.dropbox.com/scl/fi/ashj66n0pe8qo88qe2vpw/Webfisher-Setup-1.2.5.exe?rlkey=pg8qjmuwe5gr6s5o70tex778m&st=1je0j0pd&dl=1",
     filename: "Webfisher-Setup-1.2.5.exe"
 };
