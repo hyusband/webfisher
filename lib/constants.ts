c1c0fbd51190534814ec7f769b2bdfb5a5d4f2ca
zlwar <meminm052@gmail.com>
2026-08-16 22:37:12 -0700

    Change download URL for Webfisher setup
    Updated download URL to enable direct download.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.dropbox.com/scl/fi/fv0s5mebl0iradzzid617/Webfisher-Setup-2.1.1.exe?rlkey=56guhhcg7db2o6oftq7cw4xby&dl=0",
+    url: "https://www.dropbox.com/scl/fi/fv0s5mebl0iradzzid617/Webfisher-Setup-2.1.1.exe?rlkey=56guhhcg7db2o6oftq7cw4xby&dl=1",
     filename: "Webfisher-Setup-1.2.5.exe"
 };
