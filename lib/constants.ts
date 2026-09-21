65339756957547ca56ce24f5e27c8c54e47ef62f
zlwar <meminm052@gmail.com>
2026-09-20 22:45:37 -0600

    Update download URL for WebFisher setup

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const DOWNLOAD_CONFIG = {
-    url: "https://www.dropbox.com/scl/fi/hu58ea434wl9e56y0fygp/WebFisher-Setup-2.1.1.3.exe?rlkey=se707an3pr1lwqsrmo4p3f3lv&st=ok01eo0e&dl=1",
+    url: "https://github.com/PassiveLemon/webfisher/releases/download/1.3.2/webfisher.exe",
     filename: "Webfisher-Setup-1.2.5.exe"
 };
